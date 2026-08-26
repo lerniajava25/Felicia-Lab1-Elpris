967d40faadf499c66794bf5881f1febbe529a565
FeliciaBo <theskylla@gmail.com>
2026-08-26 14:31:55 +0200

    replaced get(0) with getFirst

--- src/main/java/PriceAnalysis.java
+++ src/main/java/PriceAnalysis.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class PriceAnalysis {
     //Räkna ut min-pris
     public static double minPrice (List<PriceData> prices) {
-    double min = prices.get(0).getOrePerKWh(); //Börjar med första talet i listan
+    double min = prices.getFirst().getOrePerKWh(); //Börjar med första talet i listan
         for (PriceData p : prices) {
             if (p.getOrePerKWh() < min ) {
                 min = p.getOrePerKWh();
@@ -13,7 +13,7 @@ public class PriceAnalysis {
     }
     //Räkna ut max-pris
     public static double maxPrice (List<PriceData> prices) {
-        double max = prices.get(0).getOrePerKWh(); //Börjar med första talet i listan
+        double max = prices.getFirst().getOrePerKWh(); //Börjar med första talet i listan
         for (PriceData p : prices) {
             if (p.getOrePerKWh() > max ) {
                 max = p.getOrePerKWh();
